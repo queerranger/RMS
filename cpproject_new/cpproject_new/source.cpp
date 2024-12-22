25a5089f027ee871d5c1ae566967912bfbfd3f91
Ranger <mnipk1243@gmail.com>
2024-12-22 13:59:14 +0500

    better than b4

--- cpproject_new/cpproject_new/source.cpp
+++ cpproject_new/cpproject_new/source.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <ctime>
-using namespace	std;
+using namespace std;
 void menu_order_bill();
 void login();
 void billing();
@@ -16,10 +16,19 @@ int TotalOrders = 0;
 int num;
 
 int main() {
+	int continuee;
+	do 
+	{
+		login();
+		system("cls");
+		menu_order_bill();
+		cout << "\nPress 1 to go to start.\nPress anyother button to quit.";
+		cin >> continuee;
+		if (continuee != 1)
+			break;
+	} while (continuee == 1);
 
-	login();
-	system("cls");
-	menu_order_bill();
+
 }
 
 void login() {
@@ -29,6 +38,7 @@ void login() {
 	char goback;
 	int key;
 	char option2;
+	system("cls");
 	do {
 		cout << "==================Welcome To RESTAURANT MANAGEMENT SYSTEM==================\n";
 	backlabel:
@@ -150,21 +160,27 @@ void login() {
 	} while (ent_user != username || ent_pass != pass);*/
 
 
-
+		file2.close();
+		
 	}
 }
 void menu_order_bill()
 {//menu items
+	const int appt_size = 4;
+	const int soup_size = 4;
+	const int mainc_size = 4;
+	const int dsrt_size = 4;
+
 	string menu_items[4] = { "[Appetizers]","[Soups]", "[Maincourse]","[Desserts]" };
-	string appetizer[3] = { "[Garlic Bread]","[Chicken Wings]","[Nachos with cheese]" };
-	string soups[3] = { "[Corn Soup]","[Hot n Sour]","[Yakhni]" };
-	string maincourse[3] = { "[Lache wala Paratha]","[Nihari]","[Daal]" };
-	string desserts[2] = { "[Kheer]","[Sohan Halwa]" };
+	string appetizer[appt_size] = { "[Garlic Bread]","[Chicken Wings]","[Nachos with cheese]","[Crackers]"};
+	string soups[soup_size] = { "[Corn Soup]","[Hot n Sour]","[Yakhni]","[Special soup]"};
+	string maincourse[mainc_size] = { "[Lache wala Paratha]","[Nihari]","[Daal]","[Chicken Karahi]"};
+	string desserts[dsrt_size] = { "[Kheer]","[Sohan Halwa]","[Mithai]","[Gajar Halwa]"};
 	//menu prices
-	int apt_price[3] = { 100,200,300 };
-	int soup_price[3] = { 100,200,300 };
-	int mainc_price[3] = { 100,200,300 };
-	int dsrt_price[2] = { 100,200 };
+	int apt_price[4] = { 100,200,300,150 };
+	int soup_price[4] = { 100,200,300,400 };
+	int mainc_price[4] = { 100,200,300,500 };
+	int dsrt_price[4] = { 100,200,150,300 };
 
 label:
 	cout << "Press:" << endl;
@@ -175,17 +191,18 @@ label:
 	char option;
 	cin >> option;
 
-	switch (option) {
+	switch (option)
+	{
 
 	case 'a':
 		system("cls");
 		cout << "----------[Appetizers]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < appt_size; i++)
 		{
 
 			cout << appetizer[i] << "    Rs." << apt_price[i] << "(Press [" << i + 1 << "] to order)";
 			cout << endl;
-			
+
 		}
 		char option_a;
 		do {
@@ -195,8 +212,14 @@ label:
 			order_prices[TotalOrders] = apt_price[num - 1];
 			TotalOrders++;
 			bill += apt_price[num - 1];
-			cout << "Do you want to order more from appetizers? Y/N: ";
-			cin >> option_a;
+			do {
+				cout << "Do you want to order more from appetizers? Y/N: ";
+				cin >> option_a;
+
+
+				if (option_a != 'N' && option_a != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_a != 'N' && option_a != 'Y');
 			if (option_a == 'N')
 				break;
 		} while (option_a == 'Y');
@@ -205,7 +228,7 @@ label:
 	case 'A':
 		system("cls");
 		cout << "----------[Appetizers]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < appt_size; i++)
 		{
 
 			cout << appetizer[i] << "    Rs." << apt_price[i] << "(Press [" << i + 1 << "] to order)";
@@ -220,17 +243,23 @@ label:
 			order_prices[TotalOrders] = apt_price[num - 1];
 			TotalOrders++;
 			bill += apt_price[num - 1];
-			cout << "Do you want to order more from appetizers? Y/N: ";
-			cin >> option_A;
-			if (option_A == 'N')
-				break;
-		} while (option_A == 'Y');
+			do {
+				cout << "Do you want to order more from appetizers? Y/N: ";
+				cin >> option_A;
 
-		break;
+
+				if (option_A != 'N' && option_A != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_A != 'N' && option_A != 'Y');
+				if (option_A == 'N')
+					break;
+			} while (option_A == 'Y');
+
+			break;
 	case 'b':
 		system("cls");
 		cout << "----------[Soups]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < soup_size; i++)
 		{
 			cout << soups[i] << "    Rs." << soup_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -243,8 +272,14 @@ label:
 			order_prices[TotalOrders] = soup_price[num - 1];
 			TotalOrders++;
 			bill += soup_price[num - 1];
-			cout << "Do you want to order more from maincourse? Y/N: ";
-			cin >> option_s;
+			do {
+				cout << "Do you want to order more from maincourse? Y/N: ";
+				cin >> option_s;
+
+
+				if (option_s != 'N' && option_s != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_s != 'N' && option_s != 'Y');
 			if (option_s == 'N')
 				break;
 		} while (option_s == 'Y');
@@ -255,7 +290,7 @@ label:
 	case 'B':
 		system("cls");
 		cout << "----------[Soups]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < soup_size; i++)
 		{
 			cout << soups[i] << "    Rs." << soup_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -267,9 +302,15 @@ label:
 			orders[TotalOrders] = soups[num - 1];
 			order_prices[TotalOrders] = soup_price[num - 1];
 			TotalOrders++;
-			bill +=  soup_price[num - 1];
-			cout << "Do you want to order more from maincourse? Y/N: ";
-			cin >> option_S;
+			bill += soup_price[num - 1];
+			do {
+				cout << "Do you want to order more from maincourse? Y/N: ";
+				cin >> option_S;
+
+
+				if (option_S != 'N' && option_S != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_S != 'N' && option_S != 'Y');
 			if (option_S == 'N')
 				break;
 		} while (option_S == 'Y');
@@ -281,7 +322,7 @@ label:
 	case 'c':
 		system("cls");
 		cout << "----------[MainCourse]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < mainc_size; i++)
 		{
 			cout << maincourse[i] << "    Rs." << mainc_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -296,8 +337,14 @@ label:
 			order_prices[TotalOrders] = { mainc_price[num - 1] };
 			TotalOrders++;
 			bill += mainc_price[num - 1];
-			cout << "Do you want to order more from maincourse? Y/N: ";
-			cin >> option_m;
+			do {
+				cout << "Do you want to order more from maincourse? Y/N: ";
+				cin >> option_m;
+
+
+				if (option_m != 'N' && option_m != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_m != 'N' && option_m != 'Y');
 			if (option_m == 'N')
 				break;
 		} while (option_m == 'Y');
@@ -307,7 +354,7 @@ label:
 	case 'C':
 		system("cls");
 		cout << "----------[MainCourse]----------" << endl;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < mainc_size; i++)
 		{
 			cout << maincourse[i] << "    Rs." << mainc_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -322,8 +369,14 @@ label:
 			order_prices[TotalOrders] = { mainc_price[num - 1] };
 			TotalOrders++;
 			bill += mainc_price[num - 1];
-			cout << "Do you want to order more from maincourse? Y/N: ";
-			cin >> option_M;
+			do {
+				cout << "Do you want to order more from maincourse? Y/N: ";
+				cin >> option_M;
+
+
+				if (option_M != 'N' && option_M != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_M != 'N' && option_M != 'Y');
 			if (option_M == 'N')
 				break;
 		} while (option_M == 'Y');
@@ -334,7 +387,7 @@ label:
 	case 'd':
 		system("cls");
 		cout << "----------[Desserts]----------" << endl;
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < dsrt_size; i++)
 		{
 			cout << desserts[i] << "    Rs." << dsrt_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -347,8 +400,15 @@ label:
 			order_prices[TotalOrders] = dsrt_price[num - 1];
 			TotalOrders++;
 			bill += dsrt_price[num - 1];
-			cout << "Do you want to order more from desserts? Y/N: ";
-			cin >> option_d;
+			do {
+				cout << "Do you want to order more from desserts? Y/N: ";
+				cin >> option_d;
+
+
+
+				if (option_d != 'N' && option_d != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_d != 'N' && option_d != 'Y');
 			if (option_d == 'N')
 				break;
 		} while (option_d == 'Y');
@@ -358,7 +418,7 @@ label:
 	case 'D':
 		system("cls");
 		cout << "----------[Desserts]----------" << endl;
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < dsrt_size; i++)
 		{
 			cout << desserts[i] << "    Rs." << dsrt_price[i] << " (Press [" << i + 1 << "] to order)";
 			cout << endl;
@@ -371,32 +431,48 @@ label:
 			order_prices[TotalOrders] = dsrt_price[num - 1];
 			TotalOrders++;
 			bill += dsrt_price[num - 1];
-			cout << "Do you want to order more from desserts? Y/N: ";
-			cin >> option_D;
+			do {
+				cout << "Do you want to order more from desserts? Y/N: ";
+				cin >> option_D;
+
+
+				if (option_D != 'N' && option_D != 'Y')
+					cout << "\nOnly enter Y or N\n";
+			} while (option_D != 'N' && option_D != 'Y');
 			if (option_D == 'N')
 				break;
 		} while (option_D == 'Y');
 
 
+		
+
 		break;
 
-	}
-	system("cls");
-	cout << "------Your order has been selected------" << endl;
-	do {
-		cout << "Press [1] to go to menu to order other items or [2] to checkout" << endl;
-		cin >> num;
-		if (num == 1)
+	default:
+		if (option != 'a' && option != 'A' && option != 'b' && option != 'B' && option != 'c' && option != 'C' && option != 'd' && option != 'D') {
+			cout << "\nInvalid Input.";
 			goto label;
-		else if (num == 2) {
-			order();
-			billing();
 		}
-		else cout << "invalid input";
-	} while (num != 1 && num != 2);
+
+	
+	}
+		system("cls");
+		cout << "------Your order has been selected------" << endl;
+		do {
+			cout << "Press [1] to go to menu to order other items or [2] to checkout" << endl;
+			cin >> num;
+			if (num == 1)
+				goto label;
+			else if (num == 2)
+			{
+				order();
+				billing();
+			}
+			else cout << "\ninvalid input\n";
+		} while (num != 1 && num != 2);
 
 
-
+	
 }
 void order()
 {
@@ -405,9 +481,16 @@ void order()
 	for (int i = 0; i < TotalOrders; i++) {
 		cout << orders[i] << "----Rs." << order_prices[i] << endl;
 	}
+	do
+	{
+		cout << "Press [1] to confirm your order and proceed\n";
+		cin >> num;
 
-	cout << "Press [1] to confirm your order and proceed\n";
-	cin >> num;
+		if (num == 1)
+			break;
+	} while (num != 1);
+
+	
 }
 
 
